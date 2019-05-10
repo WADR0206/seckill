71017f75dc644aeb4b8689fd5b6bb4b4b8221009
chenghongling <914305233@qq.com>
2019-05-10 21:30:09 +0800

    init

---
+++ entity/src/main/java/com/chl/entity/User.java
@@ -0,0 +1,61 @@
+package com.chl.entity;
+
+import java.util.Date;
+
+public class User {
+
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public User setCreateTime(Date createTime) {
+        this.createTime = createTime;
+        return this;
+    }
+}
